21a5a387559b88e94b357aa0f4daec0eaa1ed8c8
locphan2207 <locphan2207@gmail.com>
2018-05-05 09:50:40 -0700

    front end added

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: [/\.(js|jsx)$/],
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
